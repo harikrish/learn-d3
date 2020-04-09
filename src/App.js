a2c92b8c2ac0b608f1824b376b1fc0a024a099aa
Harikrishnan Anandan <harikrish@gmail.com>
2020-04-09 16:24:44 -0700

    Layout home page using Material Design Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
 
 import BarChart from './components/barchart';
@@ -6,23 +16,65 @@ import BarChartPart2 from './components/barchartPart2';
 import Circle from './components/circle';
 import ScatterPlot from './components/scatterPlot';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header>
-        <a
-          className="App-link"
-          href="https://github.com/d3/d3/wiki/Tutorials"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn D3
-        </a>
-      </header>
-      <BarChart/>
-      <BarChartPart2/>
-      <Circle/>
-      <ScatterPlot/>
+    <div className={classes.root}>
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Learn D3
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm">
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <BarChart />
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+              <BarChartPart2 />
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+              <Circle />
+              </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+              <ScatterPlot />
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
